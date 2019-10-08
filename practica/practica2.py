1e9ecc4d53096f34894b7ff678248044dc688ee3
SantiagoDGarcia <santiagodaivid@gmail.com>
2019-10-08 11:52:41 -0500

    Ejercicios practicos

--- practica/practica2.py
+++ practica/practica2.py
@@ -5,5 +5,5 @@
 """
 # Se asigna una cadena a la funcion y se transforma a mayusculas en el mismp
 cadena_personalizada = lambda x,y: ("%s capital de %s" % (x.upper(),y.upper()))
-# Se imprime
+# Se imprime la cadena con la funcion 
 print(cadena_personalizada("Cuenca", "Azuay"))
--- practica/practica5.py
+++ practica/practica5.py
@@ -4,7 +4,7 @@
 
 """
 datos = [(10,2), (8,7), (5,4), (3,11), (10,11)]
-
+# Se reduce la lista a sus tuplas para determinar sus términos
 reduccion = lambda x: x[0]
 raiz = lambda x: x[0]**0.5
 # Se establece el print de la primera funcion el cual pide la raiz del primer termino
