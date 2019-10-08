ed7c3268a21f113572e7e072a30ebdafe732cc2b
SantiagoDGarcia <santiagodaivid@gmail.com>
2019-10-08 10:30:34 -0500

    Uso de funcion Lambda con Tuplas

--- ejercicio6.py
+++ ejercicio6.py
@@ -6,8 +6,6 @@ lista = [10, 2, 3, 5, 1]
 
 funYear = lambda x: x[0]
 funEstatura = lambda x: x[1]
-# Se usa primeramente la fun Dato para encontrar la 3ra poscicion dentro de la tupla
-# Luego se usa la 2da funcion para encontrar la 1ra poscion dentro de la tupla anterior seleccionada
 
 funciones = lambda x: x+100
 print(min(list(map(funciones, lista))))
